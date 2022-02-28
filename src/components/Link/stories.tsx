c0007d65fd848dfa79915496dc60e9e01c7bab51
Johan <johan@Johans-MacBook-Pro.local>
2022-02-28 17:06:52 -0300

    story: create Link component

---
+++ src/components/Link/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Link from '.';
+
+export default {
+  title: 'Link',
+  component: Link
+} as Meta;
+
+export const Default: Story = () => <Link />;
